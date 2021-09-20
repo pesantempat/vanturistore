2059e937640f63b8f768057f1c4b576e67f505db
ibrahim latief <latiefmaulibra@gmail.com>
2021-09-21 02:07:37 +0700

    update email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://mybisniscepat.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://mybisniscepat.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
